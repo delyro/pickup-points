1546bf0c0441dc11bf1977d0350caf1bae3f4a96
delyro <tmalinowski.it@gmail.com>
2025-02-06 00:15:31 +0100

    Use the fastest hashing when saving data to cache
    Introduce `HasherInterface` and `Xxh3Hasher` for generating cache keys.

--- src/External/Easypack/EasypackHttpClient.php
+++ src/External/Easypack/EasypackHttpClient.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace App\External\Easypack;
 
 use App\External\Easypack\Response\Response;
+use App\Service\Hasher\HasherInterface;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class EasypackHttpClient implements EasypackClientInterface
@@ -14,7 +17,9 @@ class EasypackHttpClient implements EasypackClientInterface
 
     public function __construct(
         private readonly HttpClientInterface $httpClient,
+        private readonly CacheInterface $cache,
         private readonly SerializerInterface $serializer,
+        private readonly HasherInterface $hasher,
         private readonly string $baseUrl,
     ) {
     }
@@ -32,8 +37,12 @@ class EasypackHttpClient implements EasypackClientInterface
 
     public function getCityPickupPoints(string $city): Response
     {
-        return $this->get(self::POINTS_RESOURCE, [
-            'city' => $city,
-        ]);
+        return $this->cache->get($this->hasher->hash($city), function (ItemInterface $item) use ($city) {
+            $item->expiresAfter(new \DateInterval('P3D'));
+
+            return $this->get(self::POINTS_RESOURCE, [
+                'city' => $city,
+            ]);
+        });
     }
 }

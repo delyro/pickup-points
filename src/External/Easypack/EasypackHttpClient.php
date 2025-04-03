fe2b3b114836552f3c6e960908df4f84e5110280
delyro <tmalinowski.it@gmail.com>
2025-04-03 13:21:57 +0200

    Refactor Easypack base URL handling with dedicated class.

--- src/External/Easypack/EasypackHttpClient.php
+++ src/External/Easypack/EasypackHttpClient.php
@@ -13,20 +13,20 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class EasypackHttpClient implements EasypackClientInterface
 {
-    private const POINTS_RESOURCE = 'points';
+    private const string POINTS_RESOURCE = 'points';
 
     public function __construct(
         private readonly HttpClientInterface $httpClient,
         private readonly CacheInterface $cache,
         private readonly SerializerInterface $serializer,
         private readonly HasherInterface $hasher,
-        private readonly string $baseUrl,
+        private readonly BaseUrl $baseUrl,
     ) {
     }
 
     public function get(string $resource, array $queryData): Response
     {
-        $url = sprintf('%s/%s', $this->baseUrl, $resource);
+        $url = sprintf('%s/%s', $this->baseUrl->asString(), $resource);
 
         $response = $this->httpClient->request('GET', $url, [
             'query' => $queryData,
